7d118b5b98f6fcb068a74daf007477d3e24d8e58
Nicolas Bakanowski <nicolasbakanowski@gmail.com>
2024-03-07 22:41:32 -0300

    feat: inicialize express server

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import productRoute from './routes/productRoute.js'
+
+const app = express()
+const port = process.env.PORT || 30000
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/product', productRoute)
+
+app.use((req, res, next) => {
+  res.status(404).send('Ruta no encontrada')
+})
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Error interno del servidor')
+})
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`)
+})
