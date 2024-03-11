c82eb4f1bd2be7b2305a1d317eb7cc2476faa8b6
Nicolas Bakanowski <nicolasbakanowski@gmail.com>
2024-03-11 02:30:05 -0300

    delete console.log()

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,10 +3,7 @@ import { encryptPassword } from "../utils/passwordUtil.js"
 
 export const createUserService = async (body) => {
     try {
-        console.log("estees el body",body)
         body.password = await encryptPassword(body.password);
-        console.log(body.password, "este es el password")
-        console.log("este es el body completo", body)
         const newUser = await createUserRepository(body);
         if (!newUser) {
             throw new Error('Failed to create user');
