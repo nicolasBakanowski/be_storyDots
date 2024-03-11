5e42c20fb0991682c9b6105d291e550d7f5f7a1a
Nicolas Bakanowski <nicolasbakanowski@gmail.com>
2024-03-11 11:38:14 -0300

    fix brands

--- src/migrations/20240308081733-add_brand_fk_in_product.cjs
+++ src/migrations/20240308081733-add_brand_fk_in_product.cjs
@@ -15,6 +15,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('products', 'id_brands')
+    await queryInterface.removeColumn('products', 'id_brand')
   },
 }
